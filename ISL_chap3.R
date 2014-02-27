14e9ad484f646ee9e37109aa5ccd62d87e2842cc
Ilya Kavalerov <ilyakavalerov@gmail.com>
2014-02-27 07:53:39 -0500

    Add in chap 3 and labs from 4

---
+++ ISL_chap3.R
@@ -0,0 +1,266 @@
+################################# LAB 3.6.2 Simple Linear Regression
+require(MASS)
+require(ISLR)
+# library() and require() are the same, but require throws errors (like bang in ruby)
+boston <- Boston
+
+response <- boston$medv
+predictor <- boston$lstat
+
+lm.fit <- lm(medv~lstat, boston)
+names(lm.fit)
+# can then access the quantities from lm.fit using $ or coef() or similar
+coef(lm.fit)
+confint(lm.fit)
+
+# produce confidence intervals and prediction intervals for the
+# prediction of medv for a given value of lstat
+predict(lm.fit, data.frame(lstat = (c(5,10,15))), interval = "confidence")
+predict(lm.fit, data.frame(lstat = (c(5,10,15))), interval = "prediction")
+
+plot(lstat, medv, col = "blue", pch = 4)
+abline(lm.fit, lwd = 3, col = "red")
+# explore all the plotting symbols:
+plot(1:20, 1:20, pch=1:20)
+
+# 4 diagnostic plots are produced when applying plot() directly to the output of lm()
+# we use par() to first split the screen up for displaying these plots
+par(mfrow = c(2,2))
+plot(lm.fit)
+
+# compute the residuals from the fit
+plot(predict(lm.fit), residuals(lm.fit))
+# or the studentized residuals
+plot(predict(lm.fit), rstudent(lm.fit))
+# indeed there is some evidence of non-linearity here, lets compute leverage statistics
+plot(hatvalues(lm.fit))
+# identify index of the largest element, works on vectors
+which.max(hatvalues(lm.fit))
+
+#################################### 3.6.3 Multiple linear Regression
+lm.fit <- lm(medv ~ lstat + age, data = boston)
+summary(lm.fit)
+# it would be cumbersome to type out all of the 13 variables in the dataset as predictors, instead:
+lm.fit <- lm(medv ~ ., data = boston)
+# compute variance inflation factors
+require(car)
+vif(lm.fit)
+?summary.lm
+
+# note that age has a high p-value, ie Pr(>|t|), 
+summary(lm.fit)
+# lets exclude age:
+lm.fit1 <- lm(medv ~ . - age, data = boston)
+# alternatively we could have excluded it from the original copy:
+lm.fit1 <- update(lm.fit, ~ . - age)
+
+# we can add two predictors and an interaction term for them simultaneously
+lm.fit <- lm(medv ~ lstat * age, data = boston)
+# or separately
+lm.fit <- lm(medv ~ lstat + age + lstat:age, data = boston)
+
+# we can make one of the predictors non-linear
+lm.fit2 <- lm(medv ~ lstat + I(lstat ^ 2))
+# we see the p-values drop to near zero with the quadratic term
+summary(lm.fit2)
+# lets use anova() to further quantify the extent to which the fit improved
+lm.fit <- lm(medv ~ lstat)
+anova(lm.fit, lm.fit2)
+# we see that the p-value is nearly zero, and that the F statistic is 135
+# we can also see that there is little pattern in the residuals
+par(mfrow = c(2,2))
+plot(lm.fit2)
+
+# maybe we want to try higher order polynomials
+lm.fit5 = lm(medv ~ poly(lstat, 5))
+summary(lm.fit5)
+# this looks better! why not even try a log
+summary(lm(medv ~ log(rm), data = boston))
+
+carseats <- Carseats
+lm.fit <- lm(Sales ~ . + Income:Advertising + Price:Age, data = carseats)
+summary(lm.fit)
+# lets see how R has dealt with the qualitative variables
+contrasts(carseats$ShelveLoc)
+# we can see that it created both a ShelveLocGood and ShelveLocMed variable
+# the positive coeficient for ShelveLocGood tells us that a good shelving location
+# has a positive effect on sales
+
+############################# Exercise question 8
+require(ISLR)
+require(Auto)
+auto <- Auto
+lm.fit <- lm(mpg ~ horsepower, auto)
+
+summary(lm.fit)
+# a
+### i. The tiny p-values for the coeficient indicate that there is a 
+# relationship between our predictor and response variables, the non-zero
+# coeficients also are a good sign of a relationship, and the F-statistic,
+# like if there was a relationship, is much greater than 1
+### ii. The two measures of accuracy for relations are the RSE (residual standard error) and R^2,
+# The RSE estimates the standard deviation of the response from the population regression line,
+# and it is less than 20% of the mean, indicating roughly that much of a percentage error. The R^2
+# statistic tells us the percentage of variability in the response that is explained by the 
+# predictors. in this case it is 60%
+### iii. The slightly negative coeficient shows that the relationship between the predictor
+# and the response is slightly negative
+### iv. For simple linear regression, a 95% confidence interval (probability that a range will contain
+# the true unknown value of a parameter) can be constructed from twice the standard error 
+# for a coeficient, the prediction interval is a little wider because it also takes into
+# account the irreducible error when calculating the relationship between the true and the
+# estimated model
+predict(lm.fit, data.frame(horsepower = (c(98))), interval = "confidence")
+# in our case the confidence interval is (23.97308, 24.96108)
+predict(lm.fit, data.frame(horsepower = (c(98))), interval = "prediction")
+# and the prediction interval is (14.8094, 34.12476)
+
+# b)
+# plotting the response and the predictor, and the regression line:
+plot(auto$horsepower, auto$mpg, col = "blue", pch = 4)
+abline(lm.fit, lwd = 3, col = "red")
+# c)
+# lets produce the diagnostic plots:
+par(mfrow = c(2,2))
+plot(lm.fit)
+# There is a little bit of a U shape in the residuals vs fitted graph, indicating that
+# there might be some non-linearity in the relationship
+
+################################################### Exercise question 9
+# a) lets make all of the possible bivariate combinations of predictors in the dataset
+require(ISLR)
+auto <- Auto
+length(names(auto))
+pairs(auto[, 1:9])
+# b) lets make a matrix of correlations between the variables, excluding the name
+cor(auto[, 1:8], auto[, 1:8])
+# c) lets do a multiple linear regression on all the variables, excluding the name
+lm.fit <- lm(mpg ~ . - name, auto)
+summary(lm.fit)
+### i. We see that the intercept coeficient is non-zero, and has a low p-value, invalidating
+# the null hypothesis. We also see several other p-values that are pretty low. The F-statistic
+# is relatively high, and the RSE is only 15% of the mean mpg
+
+lm.fit2 <- lm(mpg ~ weight + year + origin, auto)
+summary(lm.fit2)
+### ii. as we pick more statistically significant parameters to fit on the model, the F-statistic doubles,
+# the RSE and the R^2 practically stay the same, and the relevant p-values stay low
+### iii. The coeficient for the year suggests that as cars get newer, their mpg increases
+plot(lm.fit2)
+# d) There is a very neat but shallow U shape in the residuals graph
+# In the residuals versus leverage plot, we can see that a number of points do have
+# a substancial amount of leverage, we can take a closer look with:
+plot(hatvalues(lm.fit2))
+
+# To detect outliers affecting R^2 or RSE's plot the studentized residuals, and pick out
+# points with residuals that are greater than 3, we can see that there a number of these
+par(mfrow = c(1,1))
+plot(predict(lm.fit2), rstudent(lm.fit2))
+
+################################### Exercise 10
+# a)
+carseats <- Carseats
+lm.fit.a <- lm(Sales ~ Population + Urban + US, carseats)
+# b)
+summary(lm.fit.a)
+# The qualitative variable US is positively related to sales of carseats in a statistically
+# significant way. The R^2 is pretty low in this case, indicating that a large percentage of
+# the data is unexplained by the 3 predictors.
+# c) Y = ß0 + ß1*Population + ß2*UrbanYes + ß3*USYes
+# d) We can reject the null hypothesis overall because of the low p-value for the intercept
+# and we can see that the USYes variable is also statistically significant.
+# e)
+lm.fit.e <- lm(Sales ~ US, carseats)
+summary(lm.fit.e)
+# the F statistic improves, but the RSE and the R^2 stay the same
+# g) the confidence interval for the coeficients will tell us how close Y-hat is to f(X)
+confint(lm.fit.e)
+# this interval tells us that the average uncertainty surrounding the average Sales over
+# US or non-US areas
+# just for fun, lets plot the confidence intervals for the prediction of Sales for the two
+# variants of US, first we can plot the data
+plot(carseats$US, carseats$Sales)
+abline(lm.fit.e, lwd = 3)
+# then, lets generate the confidence intervals for the two variants of US, and plot the lines
+predictions <- predict(lm.fit.e, data.frame(US = c("No", "Yes")), interval = "confidence")
+lm.lwr <- lm(predictions[, 2] ~ c("No", "Yes"))
+lm.upr <- lm(predictions[, 3] ~ c("No", "Yes"))
+abline(lm.lwr, col = "red")
+abline(lm.upr, col = "blue")
+
+# we can see what looks like a couple of outliers on the historgram, lets
+# see what else we can see by plotting the studentized residuals
+plot(predict(lm.fit.e), rstudent(lm.fit.e))
+# it actually doesn't look so bad, there are no points that are so far from a
+# studentized residual of 3 or -3. (Studentized residuals are calculated by dividing
+# each residual by its estimated standard error, the SE is the amount that
+# the estimated value differs from the actual value)
+############################ Exercise 11
+# lets generate a predictor and a response
+set.seed(1)
+x <- rnorm(10000)
+y <- 2 * x + rnorm(10000)
+# a) lets perform a SLR without an intercept
+lm.fit <- lm(y ~ x + 0)
+summary(lm.fit)
+# THe SE is small, and the coeficient is much (20x) larger than the SE, pointing to a relationship
+# the t value, ie the t-statistic; ß / SE(ß), is pretty high, and the p-value is low, also
+# signifying a relationship
+
+# b) performing a regresssion the other way:
+lm.fit2 <- lm(x ~ y + 0)
+summary(lm.fit2)
+# in this case, the SE is still 5% of the Coeficient, the t-value and the p-value are still low
+# the F statistic and R^2 are identical, and the RSE has been halved
+# c) The relationship between the fits is the ratio of the slopes ßx/ßy
+# from equation 3.38 we see that this is: (sum(x * y) / sum(x * x)) / (sum(y * x) / sum(y * y))
+# which equals: sum(y * y) / sum(x * x) == 4.93 or 5
+# this is the ratio of the variances of the two distributions.
+# x ~ N(0, 1)
+# y ~ N(0, 5)
+# d) substitute in ß into SE(ß) and then multiply that by ß as well
+# e) looking at the equation in 11d, because it is highly symmetric, ie switching out the
+# x's for the y's would yeild the same exact equation, the t-statistic is identically calculated
+# for a y on x or x on y regression
+# f) the t-statistic is indeed the same for ß1
+lm.fit3 <- lm(y ~ x)
+lm.fit4 <- lm(x ~ y)
+summary(lm.fit3)
+summary(lm.fit4)
+######################################## Exercise 12
+# a) The coeficient will be the same when the variance of the two are the same
+# b) This is the same as question 11
+# c) 
+set.seed(1)
+x <- rnorm(100)
+y <- rnorm(100)
+lm.fit <- lm(y ~ x + 0)
+lm.fit1 <- lm(x ~ y + 0)
+summary(lm.fit)
+summary(lm.fit1)
+######################################## Exercise 13
+x <- rnorm(100)
+# b) random noise with variance of 1/4 (variance will be the coeficient squared)
+epx <- .25 * rnorm(100)
+# c) a length 100 vector with ß0 = -1 and ß1 = 1/2
+y <- -1 + 0.5 * x + epx
+# d)
+plot(x, y)
+# e)
+lm.fit <- lm(y ~ x)
+summary(lm.fit)
+# the model provides very accurate fits for the coeficients, but about half of the 
+# data is unexplained as seen in the R^2 statistic
+# f)
+abline(lm.fit)
+abline(-1, 0.5, col = "red")
+legend("bottomleft", legend = c("estimated", "population"), text.col = 1:2)
+poly.fit <- lm(y ~ x + I(x^2))
+abline(poly.fit)
+summary(poly.fit)
+# the R^2 statistic doesn't change and the p-value is huge, not an improvement, plotting
+# even reveals that the polynomial coeficient didn't change the shape of the fit
+# h) less noise: makes the fit stronger, R^2 and F-statistic gets larger, while RSE shrinks
+# i) more noise: when the variance of the noise increases by a factor of 64, almost no
+# pattern is discernable, the null hypothesis cannot be proven wrong very confidently
+# h) after looking at the same distribution with less noise
---
+++ ISL_chap4.R
@@ -0,0 +1,108 @@
+############################## Lab 4.6.1
+library(ISLR)
+names(Smarket)
+# market data for 5 years
+table(Smarket$Year)
+dim(Smarket)
+summary(Smarket)
+pairs(Smarket)
+cor(Smarket[, -9])
+plot(Smarket$Volume)
+
+# we will specify logistic regression within the generalized linear model package
+glm.fit <- glm(Direction ~ . - Today - Year, Smarket, family = binomial)
+# even Lag1's p-value is too large to be point to a convincing relationship
+summary(glm.fit)
+
+attach(Smarket)
+# see what dummy variables R has assigned
+contrasts(Direction)
+
+glm.probs <- predict(glm.fit, type = "response")
+head(glm.probs)
+
+# lets make these predicted probabilities of the market direction bivariate
+# by first making a vector with all "Down"s and then correcting others to "Up"
+glm.pred <- rep("Down", 1250)
+glm.pred[glm.probs > 0.5] = "Up"
+# easy now to generate a confusion matrix
+# the sum of the left col == Direction$Down, and sum of right col == Direction$Up
+# 507 and 145 days were correctly predicted as Up and Down
+# the off-diagonals show the incorrect predictions
+table(glm.pred, Direction)
+# can easily see the overlap between glm.pred and Direction, the percentage of correct predictions
+# ie the precent TRUE in the vector generated inside the parentheses
+mean(glm.pred == Direction)
+# the training error rate is then 1 - this
+
+# since this is overly optimistic, lets repeat this procedure but hold out some training data
+train <- (Year < 2005)
+# train is a boolean vector (of TRUEs and FALSEs) depending on the year of the Smarket
+table(train)
+# and lets store the data but leave out the year 2005
+Smarket.2005 <- Smarket[!train, ]
+Direction.2005 <- Direction[!train]
+# we could either create another data.frame with the other Smarket data, or...
+glm.fit <- glm(Direction ~ . - Year - Today, Smarket, subset = train, family = "binomial")
+glm.probs <- predict(glm.fit, Smarket.2005, type = "response")
+# lets do the same comparrison as above
+glm.pred <- rep("Down", 252)
+glm.pred[glm.probs > 0.5] = "Up"
+table(glm.pred, Direction.2005)
+# computing the test error rate
+mean(glm.pred != Direction.2005)
+
+######################################################## LDA 4.6.3
+library(MASS)
+lda.fit <- lda(Direction ~ Lag1 + Lag2, data = Smarket, subset = train)
+# π1 = 0.492 (49% of training observations were on down days) and π2 = 0.508 here
+# the group means are the average of each predictor within each class (estimate of µk),
+# they indicate that there is a tendency for market to go Down when previous day had positive
+# returns, and up when previous day had negative returns
+# the "coefficients of linear discriminants" gives the linear  combination of the two 
+# predictors that is used to form the LDA decision rule (-0.642 * Lag1 - 0.514 * Lag2 = Pr[market_increase])
+lda.fit
+# compute linear discriminatnts (-0.642 * Lag1 - 0.514 * Lag2) for each observation and plot
+plot(lda.fit)
+lda.pred <- predict(lda.fit, Smarket.2005)
+# second element, "posterior", is a matrix whose kth column contrains the posterior Pr that the
+# observation belongs to the kth class
+names(lda.pred)
+# if we want to predict a market decrease only if the posterior Pr is 90% or more
+sum(lda.pred$posterior[, 1] > 0.9)
+# we can see that no days meet that threshold
+############################################################ KNN
+library(class)
+# requires 1) matrix containing predictors for the training data, 2) matrix with
+# predictors for the data for which we predict 3) vector with class labels for training
+# observations,4) a value for K
+# lets column bind Lag1 and Lag2 variables together in two matrices, training and test
+train.X <- cbind(Lag1, Lag2)[train, ]
+test.X <- cbind(Lag1, Lag2)[!train, ]
+train.Direction <- Direction[train]
+# lets set a random seed so that ties in our Pr(Class) are broken in a reproducible way
+knn.pred <- knn(train.X, test.X, train.Direction, k = 1)
+table(knn.pred, Direction.2005)
+# (83 + 43) / 252 == 0.5  # the percent of predictions that were correct
+# increasing K to 3 yeilds slightly better performance,but thats as good as it gets
+
+###################################### KNN for insurance
+# we standardize our variables to a mean of 0 and a std dev of 0, and exclude the Purchas variable
+standardized.X <- scale(Caravan[, -86])
+var(Caravan[, 1])
+var(standardized.X[, 1])
+# now lets split into train and test
+test <- 1:1000
+train.X <- standardized.X[-test, ]
+test.X <- standardized.X[test, ]
+train.Y <- Purchase[-test]
+test.Y <- Purchase[test]
+set.seed(1)
+knn.pred <- knn(train.X, test.X, train.Y, k = 1)
+# knn error rate
+mean(test.Y != knn.pred)
+# this might seem like an okay error rate, but considering that only 6% of the customers
+# purchased insurance, always predicting "No" would halve the error to 6%
+mean(test.Y != "No")
+# we may be more interested in the fraction of customers whose purchase was correctly predicted "Yes"
+table(knn.pred, test.Y)
